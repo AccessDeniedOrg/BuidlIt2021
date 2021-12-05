5ec13fabd46e927dc7c6cb5abe56a726b5807848
unknown <tarang.padia2@gmail.com>
2021-12-06 01:20:52 +0530

    Checkout Page

--- client/src/components/User/Donation/CheckoutPage.js
+++ client/src/components/User/Donation/CheckoutPage.js
@@ -8,6 +8,7 @@ import './css/CheckoutPage.css'
 const CheckoutPage = (props) => {
 
     const [amount, setAmount] = useState("")
+    const [step, setStep] = useState(1)
     const [error, setError] = useState({})
 
     let errorHandlerObj = {
@@ -37,56 +38,6 @@ const CheckoutPage = (props) => {
         if (errorHandlerObj['amountError'] === "") {
             e.preventDefault()
             console.log("Setting up stripe payment...")
-            // await axios.post(`${process.env.REACT_APP_BACKEND_API}/transactions/create-order`, {
-            //     amount: amount * 100
-            // }).then((res) => {
-            //     orderid = res.data.order.id
-            //     console.log(orderid)
-            //     console.log(res)
-            //     var options = {
-            //         "key": process.env.REACT_APP_RAZORPAY_KEY_ID,
-            //         "amount": amount * 100,
-            //         "currency": "INR",
-            //         "name": "The Funding Network",
-            //         "description": "Donation",
-            //         "image": logo,
-            //         "order_id": orderid,
-            //         "handler": async (response) => {
-            //             console.log(response.razorpay_order_id)
-            //             await axios.post(`${process.env.REACT_APP_BACKEND_API}/transactions/get-order`, {
-            //                 orderid: response.razorpay_order_id
-            //             }).then(async (res) => {
-            //                 await axios.post(`${process.env.REACT_APP_BACKEND_API}/transactions/store-transaction`, {
-            //                     order_id: response.razorpay_order_id,
-            //                     recepient: props.charity.name,
-            //                     email: window.localStorage.getItem("email"),
-            //                     amount: res.data.amount
-            //                 }).then(async (res) => {
-            //                     console.log(res)
-            //                     await axios.post(`${process.env.REACT_APP_BACKEND_API}/donation/update-charity`, {
-            //                         id: props.charity._id,
-            //                         funds: res.data.amount
-            //                     }).then((res) => {
-            //                         console.log(res)
-            //                     })
-            //                     window.location.href = '/donate'
-            //                 })
-            //             })
-            //         },
-            //         "prefill": {
-            //             "name": window.localStorage.getItem("username"),
-            //             "email": window.localStorage.getItem("email"),
-            //         },
-            //         "theme": {
-            //             "color": "#3399cc"
-            //         }
-            //     };
-            //     var rzp1 = new window.Razorpay(options);
-            //     rzp1.on('payment.failed', function (response) {
-            //         console.log("Payment Failed")
-            //     });
-            //     rzp1.open();
-            // })
         } else {
             setError({ ...errorHandlerObj })
         }
@@ -95,55 +46,54 @@ const CheckoutPage = (props) => {
 
     return (
         <>
-            <div style={{ margin: "120px" }}>
-                <Card className="checkout-page" style={{ marginBottom: "30px" }}>
-                    <Card.Header className="checkout-header text-center"><div className="checkout-heading">CHECKOUT</div></Card.Header>
-                    <Card.Body className="text-center">
-                        <div className="row">
-                            <div className="col-12">
+            <div style={{ margin: "155px 150px 0 150px" }}>
+                <Card className="checkout-page">
+                    <Card.Header style={{ borderBottom: "none", paddingTop: "30px", paddingRight: "30px", paddingLeft: "30px" }} className="checkout-header">
+                        <span className="checkout-heading">Add Donation Amount</span>
+                        <span style={{ float: "right", marginTop: "0" }} className="checkout-heading">1/3</span>
+                    </Card.Header>
+                    <Card.Body>
+                        <div style={{ padding: "0px" }} className="row">
+                            <div className="col-6">
                                 <div className="d-flex justify-content-center">
-                                    <img style={{ width: "25%" }} src={props.charity.logo} alt="charity_logo" />
+                                    <img style={{ width: "200px", height: "200px", objectFit: "contain" }} src={props.charity.logo} alt="charity_logo" />
+                                </div>
+                            </div>
+                            <div style={{ paddingTop: "30px" }} className="col-6">
+                                <br />
+                                <div style={{ marginTop: "7px" }}>
+                                    <span style={{ float: "left" }}><b>Charity Name: </b></span>
+                                    <span className="charity-data-right">{props.charity.name}</span>
                                 </div>
                                 <br />
-                                <div className="d-flex justify-content-center" style={{ marginTop: "30px" }}>
-                                    <div className="col-6">
-                                        <span style={{ float: "left" }}><b>Charity Name: </b></span>
-                                        <span style={{ float: "right" }}>{props.charity.name}</span>
-                                    </div>
+                                <div style={{ marginTop: "7px" }}>
+                                    <span style={{ float: "left" }}><b>Campaign Name: </b></span>
+                                    <span className="charity-data-right">{props.charity.title}</span>
                                 </div>
                                 <br />
-                                <div className="d-flex justify-content-center" style={{ marginTop: "30px" }}>
-                                    <div className="col-6">
-                                        <span style={{ float: "left" }}><b>Campaign Name: </b></span>
-                                        <span style={{ float: "right" }}>{props.charity.title}</span>
-                                    </div>
+                                <div style={{ marginTop: "7px" }}>
+                                    <span style={{ float: "left" }}><b>Campaign Description: </b></span>
+                                    <span className="charity-data-right">{props.charity.description}</span>
                                 </div>
-                                <br />
-                                <div className="d-flex justify-content-center">
-                                    <div className="col-6">
-                                        <span><b>Campaign Description </b></span>
-                                        <span style={{ float: "right" }}>{props.charity.description}</span>
-                                    </div>
-                                </div>
-                                <br />
-                                <hr />
-                                <br />
-                                <div className="d-flex justify-content-center">
-                                    <b>Please enter the amount to donate (in INR)</b>
-                                </div>
-                                <div className="d-flex justify-content-center">
-                                    <input style={{ width: "30%", height: "40px" }} value={amount} onChange={handleAmountChange}></input>
-                                </div>
-                                {
-                                    error['amountError'] === "" ? <></> : <div style={{ color: "red" }}>{error['amountError']}</div>
-                                }
                                 <br />
                             </div>
                         </div>
+                        <hr />
+                        <br />
+                        <br />
+                        <div className="d-flex justify-content-center">
+                            <b>Please enter the amount to donate (in USD)</b>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <input style={{ width: "30%", height: "40px" }} value={amount} onChange={handleAmountChange}></input>
+                        </div>
+                        {
+                            error['amountError'] === "" ? <></> : <div style={{ color: "red" }}>{error['amountError']}</div>
+                        }
                         <br />
                         <div className="d-flex justify-content-center">
                             <button onClick={props.handleBackToDonationPage} className="me-btn" style={{ float: "left", marginRight: "10px" }}>Back</button>
-                            <button onClick={handleDonation} className="me-btn" style={{ float: "right" }}>Proceed to Donate</button>
+                            <button onClick={handleDonation} className="me-btn" style={{ float: "right" }}>Next</button>
                         </div>
                     </Card.Body>
                 </Card>
