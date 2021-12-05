4cafbc678e6841b4fa9af739b5a0a4df19ff0c31
sanchi-shirur4 <sanchi.shirur4@gmail.com>
2021-12-05 20:52:26 +0530

    Wallet address generation

--- client/src/assets/js/createWallet.js
+++ client/src/assets/js/createWallet.js
@@ -1,13 +1,11 @@
-import { sha3_256 } from 'js-sha3';
+import { sha3_256 } from "js-sha3";
 
 class WalletGenerator {
-
-    generateWalletAddress(email) {
-        const hexHash = sha3_256(email)
-        const ethereumAddress = "0x" + hexHash.slice(hexHash.length - 40)
-        return ethereumAddress;
-    }
-
+	generateWalletAddress(email) {
+		const hexHash = sha3_256(email);
+		const ethereumAddress = "0x" + hexHash.slice(hexHash.length - 40);
+		return ethereumAddress;
+	}
 }
 
-export default new WalletGenerator()
+export default new WalletGenerator();
